45a984361dc374ab3e037c80d79ce4ca8eb4e6a5
ChefDeProjet <abdelkader.moussa@outlook.fr>
2019-04-30 14:00:11 +0200

    update structure

---
+++ createApi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+php artisan make:migration create$1s_table --create=$1s
+php artisan make:seeder ${1^}sTableSeeder
+php artisan make:model ${1^}
+php artisan make:factory ${1^}Factory
+php artisan make:request Store${1^}Request
+php artisan make:controller ${1^}Controller --resource
+php artisan make:resource ${1^}
+php artisan migrate:fresh
+php artisan db:seed
